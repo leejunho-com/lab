6da31981a152e949ae441f4174f251c9def2cc90
leejunho-com <91662691+leejunho-com@users.noreply.github.com>
2024-05-26 14:27:49 +0900

    Update pil.py

--- py/pil/pil.py
+++ py/pil/pil.py
@@ -1,28 +1,37 @@
 import os
 from PIL import Image, ImageCms
 
-# 사용자로부터 이미지 파일 경로 입력 받기
-image_path = input("Enter the path of the image file: ")
+class ImageProcessor:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.image = Image.open(image_path)
 
-# 이미지 파일 열기
-img = Image.open(image_path)
+    def remove_gps_info(self):
+        if input("GPS 정보를 삭제하시겠습니까? (y/n)") == 'y':
+            if 'exif' in self.image.info:
+                exif_dict = Image._getexif(self.image)
+                if 34853 in exif_dict:  # "34853" is a pointer to the GPS information IFD
+                    del exif_dict[34853]
+                self.image.info['exif'] = Image._makeexif(exif_dict)
 
-# 이미지를 RGB 모드로 변환
-img = img.convert('RGB')
+    def remove_all_exif_data(self):
+        if input("모든 EXIF 데이터를 삭제하시겠습니까? (y/n)") == 'y':
+            self.image.info.pop('exif', None)
 
-# 컬러 프로파일 확인
-if 'icc_profile' in img.info:
-    icc_profile = img.info['icc_profile']
-else:
-    # 컬러 프로파일이 없을 경우 sRGB로 설정
-    srgb_profile = ImageCms.createProfile("sRGB")
-    icc_profile = ImageCms.getOpenProfile(srgb_profile).tobytes()
+    def assign_srgb_profile(self):
+        icc_profile = self.image.info.get("icc_profile")
+        if icc_profile is None:
+            if input("ICC 프로필이 없습니다. sRGB 프로필을 할당하시겠습니까? (y/n)") == 'y':
+                srgb_profile = ImageCms.createProfile("sRGB")
+                self.image.info["icc_profile"] = ImageCms.ImageCmsProfile(srgb_profile).tobytes()
 
-# 파일 이름과 확장자 분리
-file_name, file_ext = os.path.splitext(image_path)
+    def save_image(self):
+        quality = int(input("저장할 이미지의 품질을 1-100 사이로 입력하세요: "))
+        self.image.save(self.image_path, 'JPEG', quality=quality)
 
-# 파일 이름에 '[pil]' 접미어 추가하고 다시 결합
-compressed_image_path = f"{file_name}[pil].jpg"
-
-# 이미지를 압축하여 저장하면서 컬러 프로파일 유지
-img.save(compressed_image_path, 'JPEG', quality=85, icc_profile=icc_profile)
+image_path = input("이미지 파일 경로를 입력하세요: ")
+processor = ImageProcessor(image_path)
+processor.remove_gps_info()
+processor.remove_all_exif_data()
+processor.assign_srgb_profile()
+processor.save_image()
