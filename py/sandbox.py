2c45d9640ca84dd03719fc27928aaa3a8d914afa
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2024-06-24 01:52:17 +0900

    Update sandbox.py

--- py/sandbox.py
+++ py/sandbox.py
@@ -1,1 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.naver.com")
+input("Press Enter to continue...")
